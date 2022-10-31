16aba9f61f095f379fb58cc6d9cf2642246217c1
Andrea Reyna <andree.reyna@gmail.com>
2022-10-31 11:05:17 -0300

    Validaciones de login y registro OK

---
+++ js/loginValidations.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+   document.getElementById("login").addEventListener('submit', validarFormulario);
+});
+
+function validarFormulario(evento) {
+      evento.preventDefault();
+      var user = document.getElementById('email').value;
+      var pass = document.getElementById('pass').value;
+      const message = document.getElementById('alert');
+
+      if (user.length == 0) {
+         message.classList.replace('invisible', 'visible');
+         message.innerHTML='Por favor, escribe tu correo electronico';
+         setTimeout(()=> message.innerHTML= "",1900);
+         setTimeout(()=> message.classList.replace('visible', 'invisible'),1900);
+         return;
+      }
+      if (pass.length == 0) {
+        message.classList.replace('invisible', 'visible');
+        message.innerHTML='Por favor, escribe tu clave';
+        setTimeout(()=> message.innerHTML= "",1900);
+        setTimeout(()=> message.classList.replace('visible', 'invisible'),1900);
+        return;
+      }
+      this.submit();
+   }
+
+
---
+++ js/registerValidations.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("register").addEventListener('submit', validarFormulario);
+ });
+ 
+ function validarFormulario(evento) {
+   evento.preventDefault();
+   const message = document.getElementById("alert");
+   var name = document.getElementById("name").value;
+   var lastname = document.getElementById("lastname").value;
+   var email = document.getElementById("email").value;
+   var pass = document.getElementById("pass").value;
+   var pass2 = document.getElementById("pass2").value;
+
+   if (name.length == 0) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, escribe tu nombre";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (lastname.length == 0) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, escribe tu apellido";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (email.length == 0) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, escribe tu correo electronico";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (email.length < 5 || !email.includes("@")) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, escribe un correo valido";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (pass.length == 0) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, escribe tu contraseña";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (pass2.length == 0) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Por favor, repite tu contraseña";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+   if (pass != pass2) {
+     message.classList.replace("invisible", "visible");
+     message.innerHTML = "Las contraseñas no coinciden";
+     setTimeout(() => (message.innerHTML = ""), 1900);
+     setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+     return;
+   }
+
+   if (!document.getElementById("check").checked) {
+    message.classList.replace("invisible", "visible");
+    message.innerHTML = "Debe aceptar los terminos y condiciones";
+    setTimeout(() => (message.innerHTML = ""), 1900);
+    setTimeout(() => message.classList.replace("visible", "invisible"), 1900);
+    return;
+   }
+   this.submit();
+ }
+ 
+ 
+ 
