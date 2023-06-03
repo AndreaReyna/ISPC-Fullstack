262d8182f4dca6242a788f4faadcb6fa5d620aa9
Fiorella Melgarejo ♥ <fiore9713@gmail.com>
2023-06-03 18:21:00 -0300

    Subo vista del estado de compra

---
+++ front/library-in47/src/app/estado-compra/estado-compra.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EstadoCompraComponent } from './estado-compra/estado-compra.component';
+
+
+
+@NgModule({
+  declarations: [
+    EstadoCompraComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EstadoCompraModule { }
---
+++ front/library-in47/src/app/estado-compra/estado-compra/estado-compra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estado-compra',
+  templateUrl: './estado-compra.component.html',
+  styleUrls: ['./estado-compra.component.css']
+})
+export class EstadoCompraComponent {
+
+}
