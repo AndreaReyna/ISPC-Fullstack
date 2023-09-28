37054fdd564a18bb37b23e7dcd883748b368b8e2
Franco Serafini <francoserafini70@gmail.com>
2023-09-28 09:57:01 -0300

    Main activity update

--- App_mobile/app/src/main/java/com/example/libreria_in_47_app/MainActivity.java
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.libreria_in_47_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
