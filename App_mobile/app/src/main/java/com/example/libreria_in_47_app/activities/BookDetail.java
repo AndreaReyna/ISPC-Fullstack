1591e1e750472c572c876a0a9b11ef5078f7218a
Juan Manuel Sanchez Fin <jmsanchezfin@gmail.com>
2023-10-21 19:43:50 -0300

    Add basic functionality to wishlist

--- App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/BookDetail.java
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/BookDetail.java
@@ -8,14 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.libreria_in_47_app.DataBaseSQLiteHelper;
 import com.example.libreria_in_47_app.R;
 
 public class BookDetail extends AppCompatActivity {
     ImageView ivRegresar;
-    private DataBaseSQLiteHelper dbHelper;
+    DataBaseSQLiteHelper dbHelper;
 
-    private Button bookAddWish;
+    private Button btnBookAddWish;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,7 +26,8 @@ public class BookDetail extends AppCompatActivity {
         setContentView(R.layout.activity_book_detail);
         dbHelper = new DataBaseSQLiteHelper(this);
 
-        bookAddWish = (Button)findViewById(R.id.bookAddWish); //boton para agregar libro a lista
+        btnBookAddWish = (Button)findViewById(R.id.bookAddWish); //boton para agregar libro a lista
+        AddBook();
 
         ivRegresar = findViewById(R.id.ivRegresar);
         ivRegresar.setOnClickListener (new View.OnClickListener(){
@@ -34,5 +37,23 @@ public class BookDetail extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    public void AddBook() {
+        btnBookAddWish.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                       boolean isAdded = dbHelper.addToWishlist(1); // pasarle como arg el ID del libro
+                        if (isAdded = true) {
+                            Toast.makeText(BookDetail.this, "Libro agregado", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(BookDetail.this, "Error al agregar Libro", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }
+        );
+
     }
 }
