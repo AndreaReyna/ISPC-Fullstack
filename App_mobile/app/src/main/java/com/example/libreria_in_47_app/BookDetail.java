6021d8fa56e20a289e4b62169640f472320a1b33
Juan Manuel Sanchez Fin <jmsanchezfin@gmail.com>
2023-10-21 16:46:59 -0300

    Add button method to wishlist

--- App_mobile/app/src/main/java/com/example/libreria_in_47_app/BookDetail.java
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/BookDetail.java
@@ -6,16 +6,24 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class BookDetail extends AppCompatActivity {
     ImageView ivRegresar;
+    private DataBaseSQLiteHelper dbHelper;
+
+    private Button bookAddWish;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+        dbHelper = new DataBaseSQLiteHelper(this);
+
+        bookAddWish = (Button)findViewById(R.id.bookAddWish); //boton para agregar libro a lista
+
         ivRegresar = findViewById(R.id.ivRegresar);
         ivRegresar.setOnClickListener (new View.OnClickListener(){
             @Override
