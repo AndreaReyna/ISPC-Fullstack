bb997e2ed2dacac2d4f0616f0724ec7a777e49a4
Juan Manuel Sanchez Fin <jmsanchezfin@gmail.com>
2023-10-21 16:46:59 -0300

    Add button method to wishlist

--- App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/AboutUsActivity.java
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/AboutUsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.libreria_in_47_app.R;
@@ -12,11 +13,17 @@ import com.example.libreria_in_47_app.R;
 public class AboutUsActivity extends AppCompatActivity {
 
     ImageView ivRegresar;
+    private DataBaseSQLiteHelper dbHelper;
+
+    private Button bookAddWish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_us);
+        dbHelper = new DataBaseSQLiteHelper(this);
+
+        bookAddWish = (Button)findViewById(R.id.bookAddWish); //boton para agregar libro a lista
         ivRegresar = findViewById(R.id.ivRegresar);
         ivRegresar.setOnClickListener (new View.OnClickListener(){
             @Override
