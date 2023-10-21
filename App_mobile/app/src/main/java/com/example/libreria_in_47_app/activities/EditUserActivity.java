453b75c65cbd97e2a605b9cacb215f37d89bae82
DiegoGuardatti <dguardatti@gmail.com>
2023-10-21 19:50:25 -0300

    creacion de la activity EditUser

---
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/EditUserActivity.java
@@ -0,0 +1,18 @@
+package com.example.libreria_in_47_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.libreria_in_47_app.R;
+
+public class EditUserActivity extends AppCompatActivity {
+    ImageView ivRegresar;
+    Button btnCerrarSesion;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
