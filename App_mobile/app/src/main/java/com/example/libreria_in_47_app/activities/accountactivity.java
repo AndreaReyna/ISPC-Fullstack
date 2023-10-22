7765f64450c830160a3eb32768e9d8a63addc29e
DiegoGuardatti <dguardatti@gmail.com>
2023-10-22 11:37:14 -0300

    prueba para mostrar daatos del usuario logeado

--- App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/accountactivity.java
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/activities/accountactivity.java
@@ -2,11 +2,14 @@ package com.example.libreria_in_47_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.libreria_in_47_app.R;
 
@@ -14,6 +17,8 @@ public class accountactivity extends AppCompatActivity {
     ImageView ivRegresar;
     Button btnCerrarSesion;
     Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,5 +47,38 @@ public class accountactivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        long userId = getLoggedUserId(this);
+        TextView TextView5 = findViewById(R.id.textView5);
+        TextView nombreTextView = findViewById(R.id.txtAcouNom);
+        TextView apellidoTextView = findViewById(R.id.txtAcouApe);
+        TextView emailTextView = findViewById(R.id.txtAcouEmail);
+        TextView passwordTextView = findViewById(R.id.txtAcouPass);
+
+        // Verifica si se ha obtenido un ID válido antes de establecerlo en el TextView
+        if (userId != -1) {
+            // Convierte el ID en una cadena si es necesario
+            String userIdStr = String.valueOf(userId);
+            String nombre = String.valueOf(nombreTextView);
+            //String apellido = getLoggedUserId(this, userId);
+            //String email = getLoggedUserId(this, userId);
+            //String password = getLoggedUserId(this, userId);
+
+            // Establece el ID del usuario en el TextView
+            TextView5.setText(userIdStr);
+            nombreTextView.setText(nombre);
+            //apellidoTextView.setText(apellido);
+            //emailTextView.setText(email);
+            //passwordTextView.setText(password);
+
+        } else {
+            // Maneja el caso en el que no se pudo obtener el ID del usuario
+            TextView5.setText("No se pudo obtener el ID del usuario");
+        }
+    }
+
+    public long getLoggedUserId(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("UserPreferences", Context.MODE_PRIVATE);
+        return sharedPreferences.getLong("userId", -1);  // Devuelve -1 si no se encuentra el ID del usuario
     }
 }
