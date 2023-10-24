c472cf963ce2766285fd7f1cffce524ece882749
DiegoGuardatti <dguardatti@gmail.com>
2023-10-24 20:17:22 -0300

    creacion de UserClass, IUser

---
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/interfaces/IUser.java
@@ -0,0 +1,12 @@
+package com.example.libreria_in_47_app.interfaces;
+
+public interface IUser {
+
+    String getNombre();
+
+    String getApellido();
+
+    String getEmail();
+
+    String getTelefono();
+}
