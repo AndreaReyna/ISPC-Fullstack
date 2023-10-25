dd285ae349b22b1e958708e90ad8bc5a950a38d2
4679 Fiorella Melgarejo <fiore9713@gmail.com>
2023-10-25 01:18:28 -0300

    actualizacion view wishlist

---
+++ App_mobile/app/src/main/java/com/example/libreria_in_47_app/WishlistActivity.java
@@ -0,0 +1,14 @@
+package com.example.libreria_in_47_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WishlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+    }
+}
